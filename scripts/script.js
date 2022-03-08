eedf8cc3bc1ff241ac74c0b938be5b1e66e9ca8c
MGCCCIST <58752547+vicgauthreaux@users.noreply.github.com>
2022-03-08 11:13:24 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
+/*
+    Student Name: Vic Gauthreaux
+    File Name: script.js
+    Date: May 25, 2022
+*/
 
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
